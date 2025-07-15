744d2cb8fead764b171739ef73e0b2cf1e44e794
vvviie <jerickogomez22@gmail.com>
2025-07-15 23:14:01 +0800

    package changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 
@@ -10,6 +13,7 @@ import { app, server } from "./lib/socket.js"
 dotenv.config();
 
 const PORT = process.env.PORT
+const _dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -20,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV==="production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);

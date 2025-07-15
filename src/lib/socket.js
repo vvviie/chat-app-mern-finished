cc414544c7b686f97b4599015f2a70a29807a5e5
vvviie <jerickogomez22@gmail.com>
2025-07-15 16:21:13 +0800

    socket updates

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -11,12 +11,23 @@ const io = new Server(server, {
   }
 });
 
+const userSocketMap = {}; // {userId: socketId}
+
+
 io.on("connection", (socket) => {
-  console.log(("A user connected", socket.id));
+  console.log("A user connected:", socket.id);
+
+  const userId = socket.handshake.query.userId;
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log(("A user disconnected", socket.id));
-  })
-})
+    console.log("A user disconnected:", socket.id);
+    delete userSocketMap[usreId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 export {io, app, server}
